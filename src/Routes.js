c14ee421c2c3d996dcf7e7a220d94d419f9bdd8e
JeonYooDeok <jyd910215@gmail.com>
2022-09-15 03:06:03 +0000

    Code Synced by DhiWise

---
+++ src/Routes.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Page1 from "pages/Page1";
+import Page from "pages/Page";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "pages/Home";
+import NotFound from "pages/NotFound";
+
+const ProjectRoutes = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/page" element={<Page />} />
+        <Route path="/page1" element={<Page1 />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default ProjectRoutes;
---
+++ src/pages/Page1/index.js
@@ -0,0 +1,208 @@
+import React from "react";
+
+import { Stack, List, Column, Img, Text, Button, Row } from "components";
+
+const Page1 = () => {
+  return (
+    <>
+      <Stack className="bg-white_A700 font-pretendardvariable xl:h-[1024px] 2xl:h-[1152px] 3xl:h-[1382px] lg:h-[895px] mx-[auto] w-[100%]">
+        <List
+          className="absolute bottom-[0] xl:gap-[10px] 2xl:gap-[11px] 3xl:gap-[13px] lg:gap-[8px] grid grid-cols-3 inset-x-[0] min-h-[auto] mx-[auto] w-[32%]"
+          orientation="horizontal"
+        >
+          <Column className="items-center lg:mb-[100px] xl:mb-[115px] 2xl:mb-[129px] 3xl:mb-[155px] w-[100%]">
+            <Img
+              src="images/img_image50_100X104.png"
+              className="lg:h-[110px] xl:h-[126px] 2xl:h-[141px] 3xl:h-[169px] w-[100%]"
+              alt="imageFifty"
+            />
+            <Img
+              src="images/img_image138.png"
+              className="lg:h-[114px] xl:h-[131px] 2xl:h-[147px] 3xl:h-[176px] xl:mt-[10px] 2xl:mt-[11px] 3xl:mt-[13px] lg:mt-[8px] lg:w-[113px] xl:w-[130px] 2xl:w-[146px] 3xl:w-[175px]"
+              alt="image138"
+            />
+            <Img
+              src="images/img_azuki3.png"
+              className="lg:h-[114px] xl:h-[131px] 2xl:h-[147px] 3xl:h-[176px] xl:mt-[10px] 2xl:mt-[11px] 3xl:mt-[13px] lg:mt-[8px] lg:w-[113px] xl:w-[130px] 2xl:w-[146px] 3xl:w-[175px]"
+              alt="azukiThree"
+            />
+            <Img
+              src="images/img_image126.png"
+              className="lg:h-[114px] xl:h-[131px] 2xl:h-[147px] 3xl:h-[176px] xl:mt-[10px] 2xl:mt-[11px] 3xl:mt-[13px] lg:mt-[8px] lg:w-[113px] xl:w-[130px] 2xl:w-[146px] 3xl:w-[175px]"
+              alt="image126"
+            />
+            <Img
+              src="images/img_image154.png"
+              className="lg:h-[114px] xl:h-[131px] 2xl:h-[147px] 3xl:h-[176px] xl:mt-[10px] 2xl:mt-[11px] 3xl:mt-[13px] lg:mt-[8px] lg:w-[113px] xl:w-[130px] 2xl:w-[146px] 3xl:w-[175px]"
+              alt="image154"
+            />
+            <Img
+              src="images/img_image133.png"
+              className="lg:h-[114px] xl:h-[131px] 2xl:h-[147px] 3xl:h-[176px] xl:mt-[10px] 2xl:mt-[11px] 3xl:mt-[13px] lg:mt-[8px] lg:w-[113px] xl:w-[130px] 2xl:w-[146px] 3xl:w-[175px]"
+              alt="image133"
+            />
+            <Img
+              src="images/img_image89.png"
+              className="lg:h-[114px] xl:h-[131px] 2xl:h-[147px] 3xl:h-[176px] xl:mt-[10px] 2xl:mt-[11px] 3xl:mt-[13px] lg:mt-[8px] lg:w-[113px] xl:w-[130px] 2xl:w-[146px] 3xl:w-[175px]"
+              alt="imageEightyNine"
+            />
+            <Img
+              src="images/img_superrare5.png"
+              className="lg:h-[114px] xl:h-[131px] 2xl:h-[147px] 3xl:h-[176px] xl:mt-[10px] 2xl:mt-[11px] 3xl:mt-[13px] lg:mt-[8px] lg:w-[113px] xl:w-[130px] 2xl:w-[146px] 3xl:w-[175px]"
+              alt="superrareFive"
+            />
+            <Img
+              src="images/img_image80.png"
+              className="lg:h-[114px] xl:h-[131px] 2xl:h-[147px] 3xl:h-[176px] xl:mt-[10px] 2xl:mt-[11px] 3xl:mt-[13px] lg:mt-[8px] lg:w-[113px] xl:w-[130px] 2xl:w-[146px] 3xl:w-[175px]"
+              alt="imageEighty"
+            />
+          </Column>
+          <Column className="items-center w-[100%]">
+            <Img
+              src="images/img_image95_80X104.png"
+              className="xl:h-[101px] 2xl:h-[113px] 3xl:h-[136px] lg:h-[88px] w-[100%]"
+              alt="imageNinetyFive"
+            />
+            <Img
+              src="images/img_image146.png"
+              className="lg:h-[114px] xl:h-[131px] 2xl:h-[147px] 3xl:h-[176px] xl:mt-[10px] 2xl:mt-[11px] 3xl:mt-[13px] lg:mt-[8px] lg:w-[113px] xl:w-[130px] 2xl:w-[146px] 3xl:w-[175px]"
+              alt="image146"
+            />
+            <Img
+              src="images/img_image57.png"
+              className="lg:h-[114px] xl:h-[131px] 2xl:h-[147px] 3xl:h-[176px] xl:mt-[10px] 2xl:mt-[11px] 3xl:mt-[13px] lg:mt-[8px] lg:w-[113px] xl:w-[130px] 2xl:w-[146px] 3xl:w-[175px]"
+              alt="imageFiftySeven"
+            />
+            <Img
+              src="images/img_image145.png"
+              className="lg:h-[114px] xl:h-[131px] 2xl:h-[147px] 3xl:h-[176px] xl:mt-[10px] 2xl:mt-[11px] 3xl:mt-[13px] lg:mt-[8px] lg:w-[113px] xl:w-[130px] 2xl:w-[146px] 3xl:w-[175px]"
+              alt="image145"
+            />
+            <Img
+              src="images/img_azuki5.png"
+              className="lg:h-[114px] xl:h-[131px] 2xl:h-[147px] 3xl:h-[176px] xl:mt-[10px] 2xl:mt-[11px] 3xl:mt-[13px] lg:mt-[8px] lg:w-[113px] xl:w-[130px] 2xl:w-[146px] 3xl:w-[175px]"
+              alt="azukiFive"
+            />
+            <Img
+              src="images/img_image124.png"
+              className="lg:h-[114px] xl:h-[131px] 2xl:h-[147px] 3xl:h-[176px] xl:mt-[10px] 2xl:mt-[11px] 3xl:mt-[13px] lg:mt-[8px] lg:w-[113px] xl:w-[130px] 2xl:w-[146px] 3xl:w-[175px]"
+              alt="image124"
+            />
+            <Img
+              src="images/img_image155.png"
+              className="lg:h-[114px] xl:h-[131px] 2xl:h-[147px] 3xl:h-[176px] xl:mt-[10px] 2xl:mt-[11px] 3xl:mt-[13px] lg:mt-[8px] lg:w-[113px] xl:w-[130px] 2xl:w-[146px] 3xl:w-[175px]"
+              alt="image155"
+            />
+            <Img
+              src="images/img_image134.png"
+              className="lg:h-[114px] xl:h-[131px] 2xl:h-[147px] 3xl:h-[176px] xl:mt-[10px] 2xl:mt-[11px] 3xl:mt-[13px] lg:mt-[8px] lg:w-[113px] xl:w-[130px] 2xl:w-[146px] 3xl:w-[175px]"
+              alt="image134"
+            />
+            <Img
+              src="images/img_image90.png"
+              className="lg:h-[114px] xl:h-[131px] 2xl:h-[147px] 3xl:h-[176px] xl:mt-[10px] 2xl:mt-[11px] 3xl:mt-[13px] lg:mt-[8px] lg:w-[113px] xl:w-[130px] 2xl:w-[146px] 3xl:w-[175px]"
+              alt="imageNinety"
+            />
+            <Img
+              src="images/img_superrare8.png"
+              className="lg:h-[114px] xl:h-[131px] 2xl:h-[147px] 3xl:h-[176px] xl:mt-[10px] 2xl:mt-[11px] 3xl:mt-[13px] lg:mt-[8px] lg:w-[113px] xl:w-[130px] 2xl:w-[146px] 3xl:w-[175px]"
+              alt="superrareEight"
+            />
+          </Column>
+          <Column className="items-center 2xl:mb-[101px] 3xl:mb-[121px] lg:mb-[78px] xl:mb-[90px] lg:mt-[17px] xl:mt-[20px] 2xl:mt-[22px] 3xl:mt-[27px] w-[100%]">
+            <Img
+              src="images/img_image78.png"
+              className="lg:h-[114px] xl:h-[131px] 2xl:h-[147px] 3xl:h-[176px] lg:w-[113px] xl:w-[130px] 2xl:w-[146px] 3xl:w-[175px]"
+              alt="imageSeventyEight"
+            />
+            <Img
+              src="images/img_image98.png"
+              className="lg:h-[114px] xl:h-[131px] 2xl:h-[147px] 3xl:h-[176px] xl:mt-[10px] 2xl:mt-[11px] 3xl:mt-[13px] lg:mt-[8px] lg:w-[113px] xl:w-[130px] 2xl:w-[146px] 3xl:w-[175px]"
+              alt="imageNinetyEight"
+            />
+            <Img
+              src="images/img_image149.png"
+              className="lg:h-[114px] xl:h-[131px] 2xl:h-[147px] 3xl:h-[176px] xl:mt-[10px] 2xl:mt-[11px] 3xl:mt-[13px] lg:mt-[8px] lg:w-[113px] xl:w-[130px] 2xl:w-[146px] 3xl:w-[175px]"
+              alt="image149"
+            />
+            <Img
+              src="images/img_image52.png"
+              className="lg:h-[114px] xl:h-[131px] 2xl:h-[147px] 3xl:h-[176px] xl:mt-[10px] 2xl:mt-[11px] 3xl:mt-[13px] lg:mt-[8px] lg:w-[113px] xl:w-[130px] 2xl:w-[146px] 3xl:w-[175px]"
+              alt="imageFiftyTwo"
+            />
+            <Img
+              src="images/img_image139.png"
+              className="lg:h-[114px] xl:h-[131px] 2xl:h-[147px] 3xl:h-[176px] xl:mt-[10px] 2xl:mt-[11px] 3xl:mt-[13px] lg:mt-[8px] lg:w-[113px] xl:w-[130px] 2xl:w-[146px] 3xl:w-[175px]"
+              alt="image139"
+            />
+            <Img
+              src="images/img_azuki4.png"
+              className="lg:h-[114px] xl:h-[131px] 2xl:h-[147px] 3xl:h-[176px] xl:mt-[10px] 2xl:mt-[11px] 3xl:mt-[13px] lg:mt-[8px] lg:w-[113px] xl:w-[130px] 2xl:w-[146px] 3xl:w-[175px]"
+              alt="azukiFour"
+            />
+            <Img
+              src="images/img_image128.png"
+              className="lg:h-[114px] xl:h-[131px] 2xl:h-[147px] 3xl:h-[176px] xl:mt-[10px] 2xl:mt-[11px] 3xl:mt-[13px] lg:mt-[8px] lg:w-[113px] xl:w-[130px] 2xl:w-[146px] 3xl:w-[175px]"
+              alt="image128"
+            />
+            <Img
+              src="images/img_image156.png"
+              className="lg:h-[114px] xl:h-[131px] 2xl:h-[147px] 3xl:h-[176px] xl:mt-[10px] 2xl:mt-[11px] 3xl:mt-[13px] lg:mt-[8px] lg:w-[113px] xl:w-[130px] 2xl:w-[146px] 3xl:w-[175px]"
+              alt="image156"
+            />
+            <Img
+              src="images/img_image137.png"
+              className="lg:h-[114px] xl:h-[131px] 2xl:h-[147px] 3xl:h-[176px] xl:mt-[10px] 2xl:mt-[11px] 3xl:mt-[13px] lg:mt-[8px] lg:w-[113px] xl:w-[130px] 2xl:w-[146px] 3xl:w-[175px]"
+              alt="image137"
+            />
+          </Column>
+        </List>
+        <Column className="absolute bottom-[0] items-center w-[100%]">
+          <div className="bg-gradient1  lg:h-[105px] xl:h-[121px] 2xl:h-[136px] 3xl:h-[163px] w-[100%]"></div>
+          <Column className="bg-white_A700 items-center lg:pb-[17px] xl:pb-[20px] 2xl:pb-[22px] 3xl:pb-[27px] w-[100%]">
+            <Column className="bg-white_A700 lg:p-[16px] xl:p-[18px] 2xl:p-[21px] 3xl:p-[25px] w-[100%]">
+              <Column className="items-center ml-[2px] rounded-radius825 w-[2%]">
+                <Img
+                  src="images/img_arrowleft.svg"
+                  className="lg:h-[18px] xl:h-[21px] 2xl:h-[23px] 3xl:h-[28px] rounded-radius50 w-[100%]"
+                  alt="arrowleft"
+                />
+              </Column>
+            </Column>
+            <Column className="3xl:mt-[10px] lg:mt-[6px] xl:mt-[7px] 2xl:mt-[8px] w-[97%]">
+              <Text className="font-normal not-italic lg:text-[26px] xl:text-[30px] 2xl:text-[33px] 3xl:text-[40px] text-gray_802 w-[auto]">
+                이메일을 입력해주세요.
+              </Text>
+              <Column className="bg-gray_50 border border-bluegray_100 border-solid lg:mt-[31px] xl:mt-[36px] 2xl:mt-[40px] 3xl:mt-[48px] xl:p-[10px] 2xl:p-[11px] 3xl:p-[13px] lg:p-[8px] rounded-bl-[0] rounded-br-[0] rounded-tl-[4px] rounded-tr-[4px] w-[100%]">
+                <Text className="font-medium xl:ml-[10px] 2xl:ml-[11px] 3xl:ml-[13px] lg:ml-[8px] lg:text-[13px] xl:text-[15px] 2xl:text-[16px] 3xl:text-[20px] text-gray_803 tracking-ls1 w-[auto]">
+                  이메일
+                </Text>
+                <Text className="font-normal mb-[2px] xl:ml-[10px] 2xl:ml-[11px] 3xl:ml-[13px] lg:ml-[8px] 3xl:mt-[11px] lg:mt-[7px] xl:mt-[8px] 2xl:mt-[9px] not-italic lg:text-[15px] xl:text-[17px] 2xl:text-[19px] 3xl:text-[23px] text-gray_400 w-[auto]">
+                  이메일 입력
+                </Text>
+              </Column>
+              <Button
+                className="font-medium lg:mt-[35px] xl:mt-[40px] 2xl:mt-[45px] 3xl:mt-[54px] lg:text-[16px] xl:text-[18px] 2xl:text-[21px] 3xl:text-[25px] text-center w-[100%]"
+                shape="CircleBorder24"
+              >
+                계속
+              </Button>
+            </Column>
+          </Column>
+        </Column>
+        <Row className="absolute bg-white_A700 items-center justify-between 3xl:p-[10px] lg:p-[6px] xl:p-[7px] 2xl:p-[8px] top-[0] w-[100%]">
+          <Text className="font-medium mb-[1px] lg:ml-[14px] xl:ml-[16px] 2xl:ml-[18px] 3xl:ml-[21px] lg:text-[13px] xl:text-[15px] 2xl:text-[16px] 3xl:text-[20px] text-black_900 tracking-ls1 w-[auto]">
+            12:30
+          </Text>
+          <Img
+            src="images/img_rewind.svg"
+            className="lg:h-[11px] xl:h-[13px] 2xl:h-[15px] 3xl:h-[17px] lg:mr-[18px] xl:mr-[21px] 2xl:mr-[23px] 3xl:mr-[28px] w-[4%]"
+            alt="rewind"
+          />
+        </Row>
+      </Stack>
+    </>
+  );
+};
+
+export default Page1;
